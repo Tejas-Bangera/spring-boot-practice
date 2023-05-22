4cf0b7c63bdb55429a3272eac2e7c1e6e72949b5
TEJASBANGERA97 <tejas.bangera12@gmail.com>
2023-05-22 19:02:43 +0530

    Created task to notes relationship

---
+++ taskmanager/src/main/java/com/scaler/taskmanager/entities/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.scaler.taskmanager.entities;
+
+import java.util.Date;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@MappedSuperclass
+@Getter
+public abstract class BaseEntity {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  @Column(name = "id", nullable = false)
+  private Integer id;
+
+  @CreatedDate
+  @Column(name = "created_at", nullable = false)
+  private Date createdAt;
+}
---
+++ taskmanager/src/main/java/com/scaler/taskmanager/entities/TaskEntity.java
@@ -0,0 +1,22 @@
+package com.scaler.taskmanager.entities;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+@Entity(name = "tasks")
+public class TaskEntity extends BaseEntity {
+  
+  @Column(name = "title", nullable = false, length = 150)
+  String title;
+
+  @Column(name = "description", nullable = true, length = 500)
+  String description;
+
+  @Column(name = "completed", nullable = false, columnDefinition = "boolean default false")
+  Boolean completed;
+
+  @Column(name = "due_date", nullable = true)
+  Date dueDate;
+}
