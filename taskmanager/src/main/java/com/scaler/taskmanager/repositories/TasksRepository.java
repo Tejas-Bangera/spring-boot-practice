1b18757fceaac3aa515005f84978e8909ea8cb74
TEJASBANGERA97 <tejas.bangera12@gmail.com>
2023-05-22 22:45:39 +0530

    Added test cases for task repository

--- taskmanager/src/main/java/com/scaler/taskmanager/repositories/TasksRepository.java
+++ taskmanager/src/main/java/com/scaler/taskmanager/repositories/TasksRepository.java
@@ -1,11 +1,20 @@
 package com.scaler.taskmanager.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.scaler.taskmanager.entities.TaskEntity;
 
 @Repository
 public interface TasksRepository extends JpaRepository<TaskEntity, Integer>{
-  
+
+  List<TaskEntity> findAllByCompleted(boolean completed);
+
+  @Query("SELECT t FROM tasks t WHERE t.completed = false AND t.dueDate < CURRENT_DATE")
+  List<TaskEntity> findAllOverdue();
+
+  List<TaskEntity> findAllByTitleContainingIgnoreCase(String titleFragment);
 }
--- taskmanager/src/test/java/com/scaler/taskmanager/repositories/TasksRepositoryTests.java
+++ taskmanager/src/test/java/com/scaler/taskmanager/repositories/TasksRepositoryTests.java
@@ -1,5 +1,6 @@
 package com.scaler.taskmanager.repositories;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
@@ -21,5 +22,25 @@ public class TasksRepositoryTests {
     var savedTask = tasksRepository.save(task);
  
     assertNotNull(savedTask);
+  }
+
+  @Test
+  public void testReadTask() {
+    TaskEntity task1 = new TaskEntity();
+    task1.setTitle("Test Task1");
+    task1.setDescription("Task1 Description");
+    task1.setCompleted(false);
+    
+    TaskEntity task2 = new TaskEntity();
+    task2.setTitle("Test Task2");
+    task2.setDescription("Task2 Description");
+    task2.setCompleted(false);
+
+    tasksRepository.save(task1);
+    tasksRepository.save(task2);
+
+    var tasks = tasksRepository.findAll();
+    assertNotNull(tasks);
+    assertEquals(2, tasks.size());
   }
 }
